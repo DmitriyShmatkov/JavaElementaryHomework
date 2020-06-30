b6d25b63411ec12b145edaa38ee1a85812efbbea
Dmitriy Shmatkov <chmatikd36@gmail.com>
2020-06-30 17:53:52 +0300

    Homework 33

---
+++ maven/src/main/java/homework33/src/main/java/com/example/demo/repository/AddressRepository.java
@@ -0,0 +1,9 @@
+package homework33.src.main.java.com.example.demo.repository;
+
+import com.example.demo.Model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Integer> {
+}
